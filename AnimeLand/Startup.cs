e6f5db63229c1446771de96ef7a6be89dc64ec28
Seaqqull <seaqqullprogramming@gmail.com>
2017-09-28 15:41:11 +0300

    First commit; Added "navbar" to partial view;

---
+++ AnimeLand/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AnimeLand.Startup))]
+namespace AnimeLand
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
